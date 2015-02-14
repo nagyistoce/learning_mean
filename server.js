e0976aa25f3af5f3bb0faf6079a3201c00fb06e5
Mirco <ablx@posteo.de>
2015-02-14 14:12:52 +0100

    Connect Hello World example

---
+++ server.js
@@ -0,0 +1,11 @@
+var connect = require('connect');
+var app = connect();
+
+var helloWorld =function(req, res, next) {
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Hello World');
+}
+
+app.use(helloWorld);
+
+app.listen(3000);
