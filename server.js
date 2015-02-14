e40c0b2ffbab7f64ac9baa27f35bc6e054d7c05c
Mirco <ablx@posteo.de>
2015-02-14 14:21:38 +0100

    Connect example finished

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ var helloWorld =function(req, res, next) {
 	res.end('Hello World');
 }
 
-app.use(helloWorld);
+var goodbyeWorld = function(req, res, next) {
+	res.setHeader('Content-Type','text/plain');
+	res.end('Goodbye World');
+}
+
+var logger = function(req, res, next) {
+	console.log(req.method, req.url);
+	next();
+}
+
+app.use(logger);
+app.use('/hello', helloWorld);
+app.use('/goodbye', goodbyeWorld);
 
 app.listen(3000);
